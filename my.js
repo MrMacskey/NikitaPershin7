25bbacde486957117a7c0d16c910b40198782c8c
NikitaPershin <85037004+MrMacskey@users.noreply.github.com>
2022-10-26 18:45:41 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,21 @@
+$('.galery').slick({
+    dots: true,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
